543aed172a289663d385ae2ae1b7cb6d506b4120
Ayoub ABOUNAKIF <gamikad9h@gmail.com>
2020-12-21 11:54:11 +0100

    Installation should be done in goinfre I guess

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ chmod 777 setup.sh
 rm -rf /Users/$USER/goinfre/.brew && git clone --depth=1 https://github.com/Homebrew/brew /Users/$USER/goinfre/.brew && export PATH=/Users/$USER/goinfre/.brew/bin:$PATH && brew update && echo "export PATH=/Users/$USER/goinfre/.brew/bin:$PATH" >> ~/.zshrc
 
 # 2 - Install Docker | Docker-Machine | Minikube | Kubectl
+cd /Users/$USER/goinfre
 brew install docker
 brew install docker-machine
 brew install minikube
