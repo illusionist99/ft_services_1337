c2d4b58bcf15cb0b86f856678a0828e966cdd21b
Ayoub ABOUNAKIF <gamikad9h@gmail.com>
2020-12-21 20:25:33 +0100

    Nginx working

--- srcs/nginx/script.sh
+++ srcs/nginx/script.sh
@@ -1,10 +1,10 @@
-echo 'Install Nginx web server on Alpine Linux'
+echo 'Installing Nginx web server on Alpine Linux'
 
 # Docker basic commands
 
 # Creating and running an image
 # --> docker build -t aabounak .
-# --> docker run -it -p 80:80 -p 443:443 -d aabounak
+# --> docker run -it -p 80:80 -p 443:443 aabounak
 # --> docker start {CONTAINER ID}
 # --> docker exec -it {CONTAINER ID} sh
 
@@ -32,21 +32,29 @@ echo 'Install Nginx web server on Alpine Linux'
 
 # Script
 
-# Step 1 : Installation
-# apk update
-# apk add nginx
+# Step 1 : Installing Nginx
+apk update ; apk add nginx
 
 # Create new user and group 'www' for nginx
-# adduser -D -g 'www' www
+adduser -D -g 'www' www
+# Web server files and permissions
+chown -R www:www /var/lib/nginx
+chown -R www:www /www
+mkdir -p /run/nginx # Solve error --> nginx: [emerg] open() "/run/nginx/nginx.pid" failed (2: No such file or directory)
+touch /run/nginx/nginx.pid
 
-# Create a directory for html files
-# mkdir /www/
-# chown -R www:www /var/lib/nginx
-# chown -R www:www /www
+# Step 3: Secure Nginx with an SSL certificate
+
+apk add openssl
+openssl req -x509 -nodes -days 365 -subj "/C=MA/ST=BG/O=aabounak, Inc./CN=localhost" -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 
 # Step 2 : Configuration
-# Make backup of original nginx.conf file
-# mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 
-# Configuring Nginx to listen to port 80 and process .html or .htm files
-# TO:DO (Will be done manually then copy it)
+# Configuring Nginx to listen to port 80 and 443 and process .html or .htm files
+# --> Configure nginx.conf on /etc/nginx
+# --> Configure index.html on /www/
+# (Already configured; Dockerfile will copy these two files to their appropriate paths.)
+
+sudo openrc reboot
+nginx
+/bin/sh
