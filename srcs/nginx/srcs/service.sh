a81f5070ef6d659788c1e634e3fbccb08e00bd1d
Abdelhakim Nassif <anassif@e2r1p10.1337.ma>
2021-01-30 18:47:50 +0100

    li mafhamch chi 9alwa yji andi

--- srcs/nginx/srcs/service.sh
+++ srcs/nginx/srcs/service.sh
@@ -1,4 +1,6 @@
-adduser anassif -D
+sudo adduser anassif
+#add user to sudoers
+echo "%anassif ALL=(ALL) ALL" >>/etc/sudoers
 echo anassif:anassif | chpasswd
 #to prevent ssh from starting right away
 rc-status
