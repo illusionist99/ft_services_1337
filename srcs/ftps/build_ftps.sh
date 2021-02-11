8f97f3ceb8cb2d08ed4ab6a7b8cfb07f366c66a8
Mohamed Alaoui <malaoui@e1r9p4.1337.ma>
2021-02-11 14:50:37 +0100

    ff

---
+++ srcs/ftps/build_ftps.sh
@@ -0,0 +1,9 @@
+minikube start
+kubectl apply -f ../metallb.yaml
+eval $(minikube docker-env)
+docker build -t my_ftps .
+minikube addons enable metallb
+
+kubectl apply -f ftps.yaml
+minikube dashboard&
+
--- srcs/ftps/script.sh
+++ srcs/ftps/script.sh
@@ -1,13 +1,15 @@
 #!bin/bash
 
 # ssl 
+adduser -D 'ftpsuser'
+echo "ftpsuser:1234" | chpasswd
 
 sudo openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
 
-sudo mkdir /home/ftpuser/ && sudo mkdir /home/ftpuser/ftp &&sudo chown -R :users /home/ftpuser/ftp
-sudo chmod a-w /home/ftpuser/ftp/
-sudo mkdir /home/ftpuser/ftp/files && sudo chown -R :users /home/ftpuser/ftp/files
+sudo mkdir /home/ftpuser/ && sudo chown -R :ftpsuser /home/ftpuser/
+sudo chmod a-w /home/ftpuser/
 
 openrc reboot
 rc-service vsftpd start
+tail -f /dev/null
 sh 
