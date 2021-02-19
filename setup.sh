087ebb823ee47267c9e5120b4ff0ee9656b9b90f
Mohamed Alaoui <malaoui@e1r10p10.1337.ma>
2021-02-19 18:30:53 +0100

    grafana

--- setup.sh
+++ setup.sh
@@ -1,21 +1,22 @@
-minikube delete
-minikube start --driver=virtualbox
+#minikube delete
+#minikube start --driver=virtualbox
 eval $(minikube -p minikube docker-env)
 
-docker build -t ftps srcs/ftps/
+#docker build -t ftps srcs/ftps/
 #docker build -t mysql srcs/mysql/
 #docker build -t nginx srcs/nginx/
 #docker build -t phpmyadmin srcs/phpmyadmin/
 #docker build -t wordpress srcs/wordpress/
+#docker build -t nginx srcs/grafana/
 
-
-minikube addons enable metallb
+#minikube addons enable metallb
 
 kubectl apply -f srcs/metallb.yaml
-kubectl apply -f srcs/ftps/ftps.yaml
+#kubectl apply -f srcs/ftps/ftps.yaml
 #kubectl apply -f srcs/wordpress/wordpress.yaml
 #kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 #kubectl apply -f srcs/nginx/nginx.yaml
 #kubectl apply -f srcs/mysql/mysql.yaml
+kubectl apply -f srcs/grafana/grafana.yaml
 
 minikube dashboard &
---
+++ srcs/grafana/script.sh
@@ -0,0 +1,7 @@
+
+grafana-server -config "/usr/share/grafana/conf/defaults.ini" -homepath "/usr/share/grafana/" &
+
+sh
+
+clear
+ 
