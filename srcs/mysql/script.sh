f77fe8e50857aa11786283dec2a71856b05fabac
Mohamed Alaoui <malaoui@e1r10p8.1337.ma>
2021-03-17 11:42:15 +0100

    done

--- srcs/mysql/script.sh
+++ srcs/mysql/script.sh
@@ -16,6 +16,7 @@ echo "FLUSH PRIVILEGES;" | mysql -u root
 
 mysql -u root -e 'GRANT SELECT, INSERT, DELETE, UPDATE ON phpmyadmin.* TO 'pma'@'%' IDENTIFIED BY "1234"'
 mysql < /create_pma.sql
+mysql < /wordpress.sql
 openrc reboot
 
 rc-service mariadb start
